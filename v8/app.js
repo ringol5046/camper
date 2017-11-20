4d3b485beec49fa405eab00fb15bb7b6e4c25468
Ringo <ringol5046@gmail.com>
2017-11-20 00:57:20 +0000

    refactor comment model

---
+++ v8/app.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var passport = require("passport");
+var localStrategy = require("passport-local");
+
+var campgroundRoutes = require("./routes/campgrounds");
+var commentRoutes = require("./routes/comments");
+var authRoutes =require("./routes/index")
+
+var User = require("./models/user")
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+var seedDB = require("./seeds");
+
+var app = express();
+
+//seedDB();
+mongoose.connect("mongodb://localhost/yelp_camp_v8");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.set('view engine', 'ejs');
+
+//===========================
+// passport setup
+//===========================
+
+app.use(require("express-session")({
+    secret: 'Esperanza gomesz',
+    resave:false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+passport.use(new localStrategy(User.authenticate()));
+
+app.use(function(req, res, next) {
+   res.locals.currentUser = req.user;
+   next();
+});
+
+app.use(authRoutes);
+app.use('/campgrounds/:id/comments', commentRoutes);
+app.use('/campgrounds', campgroundRoutes);
+
+app.listen(process.env.PORT, process.env.IP, () => {
+   console.log(`Server start on ${process.env.PORT}`); 
+});
